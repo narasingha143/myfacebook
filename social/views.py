b1d7a5127ba9ea0d2d63a2781ba0befe43c93c27
narasingha143 <narasinghasethy32@gmail.com>
2022-06-30 17:24:02 +0530

    fourth commit

--- social/views.py
+++ social/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 
 from django.contrib.auth.decorators import login_required
-from sympy import content
  
 from .models import UserPost
 
